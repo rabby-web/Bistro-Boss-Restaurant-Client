caf7722d4b1ef7fb5729c8ec8115395b3f58ef91
rabby-web <rabby.webdeveloper@gmail.com>
2024-08-06 14:12:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="avatar">
+        <div className="w-24 rounded">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
