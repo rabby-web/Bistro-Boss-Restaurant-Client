3ccc9d81fe6d785ca2380fcb10353fee762ef8ac
rabby-web <rabby.webdeveloper@gmail.com>
2024-08-07 22:56:35 +0600

    featured section add home page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,7 +2,9 @@
 
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import Featured from "../Featured/Featured";
 import PopularMenu from "../PopularMenu/PopularMenu";
+import Testimonials from "../Testimonials/Testimonials";
 
 const Home = () => {
   return (
@@ -10,6 +12,8 @@ const Home = () => {
       <Banner></Banner>
       <Category></Category>
       <PopularMenu></PopularMenu>
+      <Featured></Featured>
+      <Testimonials></Testimonials>
     </>
   );
 };
---
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const Testimonials = () => {
+  return (
+    <div>
+      <SectionTitle
+        subHeading="What Our Client Say"
+        heading="Testimonials"
+      ></SectionTitle>
+    </div>
+  );
+};
+
+export default Testimonials;
