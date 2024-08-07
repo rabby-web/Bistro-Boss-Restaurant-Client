78f3776a78ee71b3160fccf353ceecd1529069d8
rabby-web <rabby.webdeveloper@gmail.com>
2024-08-07 23:15:27 +0600

    testimonial add

--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -1,12 +1,46 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+// import required modules
+import { Navigation } from "swiper/modules";
+import { useEffect, useState } from "react";
 
 const Testimonials = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
   return (
-    <div>
+    <div className="my-20">
       <SectionTitle
+        className="mb-10"
         subHeading="What Our Client Say"
         heading="Testimonials"
       ></SectionTitle>
+      <div>
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          {reviews.map((review) => (
+            <SwiperSlide key={review._id}>
+              <div className="flex flex-col items-center mx-24 my-16">
+                <Rating
+                  style={{ maxWidth: 180 }}
+                  value={review.rating}
+                  readOnly
+                />
+                <p className="py-8">{review.details}</p>
+                <h3 className="text-2xl text-orange-500">{review.name}</h3>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
