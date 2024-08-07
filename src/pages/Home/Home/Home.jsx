c68d638ccd5d9dca83102cf6af8462377f2bd3ee
rabby-web <rabby.webdeveloper@gmail.com>
2024-08-07 20:53:14 +0600

    popolar item menu add

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,12 +2,14 @@
 
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import PopularMenu from "../PopularMenu/PopularMenu";
 
 const Home = () => {
   return (
     <>
       <Banner></Banner>
       <Category></Category>
+      <PopularMenu></PopularMenu>
     </>
   );
 };
