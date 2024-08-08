b1da1df97b32985659380fb4b6c753f5c02b9d2a
rabby-web <rabby.webdeveloper@gmail.com>
2024-08-08 10:01:04 +0600

    costom hooks usemenu use

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImag from "../../../assets/menu/banner3.jpg";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
   return (
@@ -6,7 +9,12 @@ const Menu = () => {
       <Helmet>
         <title>Bistro-Boss || Menu</title>
       </Helmet>
-      <h3>Menu</h3>
+      <Cover img={menuImag} title="Our Menu"></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={menuImag} title="Our Menu"></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={menuImag} title="Our Menu"></Cover>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
---
+++ src/pages/Shared/Cover/Cover.jsx
@@ -0,0 +1,29 @@
+import { Parallax } from "react-parallax";
+
+const Cover = ({ img, title }) => {
+  return (
+    <Parallax
+      blur={{ min: -60, max: 60 }}
+      bgImage={img}
+      bgImageAlt="Our Menu"
+      strength={-200}
+    >
+      <div className="hero h-[650px]">
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </Parallax>
+  );
+};
+
+export default Cover;
