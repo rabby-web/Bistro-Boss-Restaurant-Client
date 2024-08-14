24a42846f1705b96827c598025e7061b22a07e3e
rabby-web <rabby.webdeveloper@gmail.com>
2024-08-14 08:54:55 +0600

    axios secure api file add access token

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,6 +4,31 @@ const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 const useAxiosSecure = () => {
+  // request interceptors to add authorization header for every secure call to the api
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      console.log("request stopped by interceptor =", token);
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  // interceptors 401 and 403 status
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    (error) => {
+      const status = error.response.status;
+      console.log("status err", status);
+      return Promise.reject(error);
+    }
+  );
+
   return axiosSecure;
 };
 
