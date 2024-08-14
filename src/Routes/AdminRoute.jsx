69997f9992a558251177b32cbd613f2fbba7ea40
rabby-web <rabby.webdeveloper@gmail.com>
2024-08-14 09:59:18 +0600

    Admin routes for secure

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -1,16 +1,18 @@
 import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../hooks/useAdmin";
 import useAuth from "../hooks/useAuth";
 
-const PrivateRoute = ({ children }) => {
-  const { user, loading } = useAuth();
+const AdminRoute = (children) => {
+  const [user, loading] = useAuth();
+  const [isAdmin, isAdminLoading] = useAdmin();
   const location = useLocation();
-  if (loading) {
+  if (loading || isAdminLoading) {
     return <span className="loading loading-ring loading-md"></span>;
   }
-  if (user) {
+  if (user && isAdmin) {
     return children;
   }
   return <Navigate to="/login" state={{ from: location }}></Navigate>;
 };
 
-export default PrivateRoute;
+export default AdminRoute;
