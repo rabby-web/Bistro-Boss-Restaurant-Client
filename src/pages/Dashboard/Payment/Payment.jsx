51128061ce8a3aa6904062d9dda97e454a782df1
rabby-web <rabby.webdeveloper@gmail.com>
2024-08-16 16:30:08 +0600

    check out from add

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,10 +1,19 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutFrom from "./CheckoutFrom";
 
+// TODO: add publishable key
+const stripePromise = loadStripe("");
 const Payment = () => {
   return (
     <div>
       <SectionTitle heading="Payment" subHeading="Pay now"></SectionTitle>
-      <div>Pay Now</div>
+      <div>
+        <Elements stripe={stripePromise}>
+          <CheckoutFrom></CheckoutFrom>
+        </Elements>
+      </div>
     </div>
   );
 };
