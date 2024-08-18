068a103e565363c24356a423f78eefdaaa12be30
rabby-web <rabby.webdeveloper@gmail.com>
2024-08-18 17:45:48 +0600

    payment history table add

--- src/pages/Dashboard/PaymentHistory/PaymentHistory.jsx
+++ src/pages/Dashboard/PaymentHistory/PaymentHistory.jsx
@@ -1,12 +1,50 @@
+import { useQuery } from "@tanstack/react-query";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const PaymentHistory = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: payments = [] } = useQuery({
+    queryKey: ["payments", user.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/payments/${user.email}`);
+      return res.data;
+    },
+  });
   return (
     <div>
       <SectionTitle
         heading="payment history"
         subHeading="Details"
       ></SectionTitle>
+      <div>
+        <h2>Total Payment: {payments.length}</h2>
+        <div className="overflow-x-auto">
+          <table className="table table-zebra">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>##</th>
+                <th>Price</th>
+                <th>Transaction Id</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {/* <tr>
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+              </tr> */}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
