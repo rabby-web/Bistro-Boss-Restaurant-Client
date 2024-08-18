c1fb26c547a9e8d864fccbd7355d3a11b3b5a6c4
rabby-web <rabby.webdeveloper@gmail.com>
2024-08-18 22:44:09 +0600

    pai and bar chart add for admin

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -2,70 +2,197 @@ import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { FaDollarSign, FaList, FaUsers } from "react-icons/fa";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  PieChart,
+  Pie,
+  ResponsiveContainer,
+  Legend,
+} from "recharts";
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
 
 const AdminHome = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
-  const { data: stats } = useQuery({
+  const { data: stats = {} } = useQuery({
     queryKey: ["admin-stats"],
     queryFn: async () => {
       const res = await axiosSecure.get("/admin-stats");
       return res.data;
     },
   });
+  const { data: chartData = [] } = useQuery({
+    queryKey: ["order-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/order-stats");
+      return res.data;
+    },
+  });
+  // custom shape for the bar chart
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${
+      x + width / 2
+    },${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${
+      x + width
+    }, ${y + height}
+    Z`;
+  };
+  // custom shape for the pai chart
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
+  const pieChartData = chartData.map((data) => {
+    return { name: data.category, value: data.revenue };
+  });
+
   return (
     <div>
-      <h2 className="text-3xl font-medium text-center">
-        <span>Hi, Welcome</span>{" "}
-        <span className="text-orange-600">
-          {user?.displayName ? user.displayName : "Back"}
-        </span>
-      </h2>
-      <hr className="my-5" />
-      <div className="">
-        <div className="stats shadow flex mx-auto">
-          <div className="stat">
-            <div className="stat-figure text-secondary text-4xl">
-              <FaDollarSign></FaDollarSign>
+      <div>
+        <h2 className="text-3xl font-medium text-center">
+          <span>Hi, Welcome</span>{" "}
+          <span className="text-orange-600">
+            {user?.displayName ? user.displayName : "Back"}
+          </span>
+        </h2>
+        <hr className="my-5" />
+        <div className="">
+          <div className="stats shadow flex mx-auto">
+            <div className="stat">
+              <div className="stat-figure text-secondary text-4xl">
+                <FaDollarSign></FaDollarSign>
+              </div>
+              <div className="stat-title text-lg font-medium">Revenue</div>
+              <div className="stat-value">$: {stats?.revenue}</div>
             </div>
-            <div className="stat-title text-lg font-medium">Revenue</div>
-            <div className="stat-value">$: {stats?.revenue}</div>
-          </div>
 
-          <div className="stat">
-            <div className="stat-figure text-secondary text-4xl">
-              <FaUsers></FaUsers>
+            <div className="stat">
+              <div className="stat-figure text-secondary text-4xl">
+                <FaUsers></FaUsers>
+              </div>
+              <div className="stat-title text-lg font-medium">Users</div>
+              <div className="stat-value">{stats?.users}</div>
             </div>
-            <div className="stat-title text-lg font-medium">Users</div>
-            <div className="stat-value">{stats?.users}</div>
-          </div>
 
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current"
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block h-8 w-8 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
+                  ></path>
+                </svg>
+              </div>
+              <div className="stat-title ext-lg font-medium">Orders</div>
+              <div className="stat-value">{stats?.orders}</div>
+            </div>
+            <div className="stat">
+              <div className="stat-figure text-secondary text-4xl">
+                <FaList></FaList>
+              </div>
+              <div className="stat-title ext-lg font-medium">Menu Items</div>
+              <div className="stat-value">{stats?.menuItems}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* chart */}
+      <div className="flex">
+        {/* 1 */}
+        <div className="w-1/2">
+          <BarChart
+            width={500}
+            height={300}
+            data={chartData}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="category" />
+            <YAxis />
+            <Bar
+              dataKey="quantity"
+              fill="#8884d8"
+              shape={<TriangleBar />}
+              label={{ position: "top" }}
+            >
+              {chartData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+              ))}
+            </Bar>
+          </BarChart>
+        </div>
+        {/* 2 */}
+        <div className="w-1/2">
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart width={400} height={400}>
+              <Pie
+                data={pieChartData}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={80}
+                fill="#8884d8"
+                dataKey="value"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
-                ></path>
-              </svg>
-            </div>
-            <div className="stat-title ext-lg font-medium">Orders</div>
-            <div className="stat-value">{stats?.orders}</div>
-          </div>
-          <div className="stat">
-            <div className="stat-figure text-secondary text-4xl">
-              <FaList></FaList>
-            </div>
-            <div className="stat-title ext-lg font-medium">Menu Items</div>
-            <div className="stat-value">{stats?.menuItems}</div>
-          </div>
+                {pieChartData.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+              <Legend></Legend>
+            </PieChart>
+          </ResponsiveContainer>
         </div>
       </div>
     </div>
